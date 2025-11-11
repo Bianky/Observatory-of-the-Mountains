fa613b88cb2b8d474ea35a2f2294116f2ee4ffd0
Bianka <bianka.fabryova@wur.nl>
2025-11-11 15:00:35 +0100

    map update

--- js/map.js
+++ js/map.js
@@ -1,9 +1,10 @@
 var map = L.map('map').setView([41.9, 2.2], 8);
 
+
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-var marker = L.marker([34.0697,-118.4432]).addTo(map)
-		.bindPopup('The Technology Sandbox<br> Where Yoh is sitting this very moment')
-		.openPopup();	
+var marker = L.marker([41.3851, 2.1734]).addTo(map)
+	.bindPopup('Hola! You are in Catalonia 🇪🇸')
+	.openPopup();
