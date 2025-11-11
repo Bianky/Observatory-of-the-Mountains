4756731ab3e71632a182ffb84a0ea6d2aa70843d
Bianka <bianka.fabryova@wur.nl>
2025-11-11 14:53:07 +0100

    map of catalunya

--- js/map.js
+++ js/map.js
@@ -1,4 +1,4 @@
-var map = L.map('map').setView([34.0697,-118.4432], 17);
+var map = L.map('map').setView([41.9, 2.2], 8);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
